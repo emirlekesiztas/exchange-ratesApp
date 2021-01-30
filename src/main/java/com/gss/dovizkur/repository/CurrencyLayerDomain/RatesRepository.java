20d6a7df0e60ceff1897e0aa23ffa71ae5984e7d
Emir <131630048@ogr.uludag.edu.tr>
2021-01-30 20:44:19 +0300

    fix

--- src/main/java/com/gss/dovizkur/repository/CurrencyLayerDomain/RatesRepository.java
+++ src/main/java/com/gss/dovizkur/repository/CurrencyLayerDomain/RatesRepository.java
@@ -1,7 +1,7 @@
 package com.gss.dovizkur.repository.CurrencyLayerDomain;
 
 import com.gss.dovizkur.domain.CurrencyLayerDomain.Rates;
-import org.springframework.data.jpa.repository.JpaRepository;;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
